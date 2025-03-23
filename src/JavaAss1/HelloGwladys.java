e9684b932d12e92f43ad03b19f922a6a17fdbec9
gnkazeu <cassy.casey2@gmail.com>
2025-03-22 23:17:21 -0400

    add assignment task4

---
+++ src/JavaAss1/HelloGwladys.java
@@ -0,0 +1,8 @@
+package JavaAss1;  // Must match folder name exactly
+
+public class HelloGwladys {
+    public static void main(String[] args) {  // Fixed 'string' to 'String'
+        System.out.println("Hello");
+        System.out.println("Gwladys");  // Added missing semicolon
+    }
+}
