5dcabef1e26364440d7f168d24205fcc056ab47b
gnkazeu <cassy.casey2@gmail.com>
2025-03-22 23:17:37 -0400

    add assign task5

---
+++ src/JavaAss1/DaysMonth.java
@@ -0,0 +1,39 @@
+package JavaAss1;
+import java.util.Scanner;
+
+public class DaysMonth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter month number (1-12): ");
+        int month = scanner.nextInt();
+        int year = 0;
+
+        if (month == 2) {
+            System.out.print("Enter year: ");
+            year = scanner.nextInt();
+        }
+
+        int days = 0;
+
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                days = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                days = 30;
+                break;
+            case 2:
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    days = 29;
+                } else {
+                    days = 28;
+                }
+                break;
+            default:
+                System.out.println("Invalid month number");
+                return;
+        }
+
+        System.out.println("Number of days: " + days);
+    }
+}
