8ac8e73a85dddc66eaab051ed8e6fa99f34d7ebf
gnkazeu <cassy.casey2@gmail.com>
2025-03-22 23:17:52 -0400

    add assign task 6

---
+++ src/JavaAss1/Greatest.java
@@ -0,0 +1,34 @@
+package JavaAss1;
+
+import java.util.Scanner;
+
+public class Greatest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("second number: ");
+        int num2 = scanner.nextInt();
+        
+        System.out.print("third number: ");
+        int num3 = scanner.nextInt();
+
+        int greatest = num1;
+
+        if (num2 > greatest) {
+            greatest = num2;
+        }
+
+        if (num3 > greatest) {
+            greatest = num3;
+        }
+
+
+        System.out.println("The greatest number is: " + greatest);
+
+        scanner.close();
+    }
+}
