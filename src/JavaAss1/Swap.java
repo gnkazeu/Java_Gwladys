dc0d2d09d9d9990005002dc7162e677c2469e304
gnkazeu <cassy.casey2@gmail.com>
2025-03-22 23:16:26 -0400

    add assigment task1

---
+++ src/JavaAss1/Swap.java
@@ -0,0 +1,21 @@
+package JavaAss1;
+
+public class Swap {
+    public static void main (String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("New value of a is " + a);
+        System.out.println("New value of b is " + b);
+
+        int c = a;
+        a = b;
+        b = c; 
+
+        System.out.println("New value of a is " + a);
+        System.out.println("New value of b is " + b);
+    
+    }
+    
+}
