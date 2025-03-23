4ac5da90b397e2f52325e6b8a90427b5cbf0727b
gnkazeu <cassy.casey2@gmail.com>
2025-03-22 23:17:03 -0400

    add assigment task3

---
+++ src/JavaAss1/Scanner_java.java
@@ -0,0 +1,12 @@
+package JavaAss1;
+
+import java.util.Scanner;
+
+public class Scanner_java {
+    public static void main(String[] args) {  
+        Scanner input  = new Scanner(System.in);
+        int number = input.nextInt();
+        System.out.println("input is " + number); 
+    }
+    
+}
