171ee2f507c151d36591efbf3686e90d8013024d
gnkazeu <cassy.casey2@gmail.com>
2025-03-22 23:16:43 -0400

    add assignement task2

---
+++ src/JavaAss1/Sum.java
@@ -0,0 +1,15 @@
+package JavaAss1;
+
+public class Sum  {
+    public static void main (String[] args) {
+
+        int a = 4;
+        int b = 36;
+        int sum;
+        sum = a + b;
+        System.out.println("Sum of a and b is " +  sum);
+    
+    }
+    
+    
+}
